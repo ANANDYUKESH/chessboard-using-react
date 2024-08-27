dc77dfba65e3c2ab353795f7e366bb6414fc18b5
ANANDYUKESH <anandyukesh003@gmail.com>
2024-08-27 17:43:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FaChessPawn,FaChessRook,FaChessKnight,FaChessBishop,FaChessKing,FaChessQueen } from "react-icons/fa6";
 
-function App() {
+function Chessboard() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <table>
+        <tr>
+            <td class="white">
+            <p><FaChessRook size={35}/></p> </td>
+            <td class="black"><p><FaChessKnight size={35} /></p></td>
+            <td class="white"><p><FaChessBishop size={35}/></p></td>
+            <td class="black"><p><FaChessKing size={35}/></p></td>
+            <td class="white"><p><FaChessQueen size={35}/></p></td>
+            <td class="black"><p><FaChessBishop size={35}/></p></td>
+            <td class="white"><p><FaChessKnight size={35}/></p></td>
+            <td class="black"><p><FaChessRook size={35}/></p></td>
+        </tr>
+        <tr>
+            <td class="black">
+            <p><FaChessPawn size={35} /></p></td>
+            <td class="white"> <p><FaChessPawn size={35} /></p></td>
+            <td class="black"><p><FaChessPawn size={35}/></p></td>
+            <td class="white"><p><FaChessPawn size={35}/></p></td>
+            <td class="black"><p><FaChessPawn size={35}/></p></td>
+            <td class="white"><p><FaChessPawn size={35}/></p></td>
+            <td class="black"><p><FaChessPawn size={35}/></p></td>
+            <td class="white"><p><FaChessPawn size={35}/></p></td>
+        </tr>
+        <tr>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+        </tr>
+        <tr>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+        </tr>
+        <tr>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+        </tr>
+        <tr>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+        </tr>
+        <tr>
+            <td class="black">
+            <p style={{color:'wheat'}}><FaChessPawn size={35} /></p></td>
+            <td class="white"> <p style={{color:'wheat'}}><FaChessPawn size={35} /></p></td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessPawn size={35}/></p></td>
+            <td class="white"><p style={{color:'wheat'}}><FaChessPawn size={35}/></p></td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessPawn size={35}/></p></td>
+            <td class="white"><p style={{color:'wheat'}}><FaChessPawn size={35}/></p></td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessPawn size={35}/></p></td>
+            <td class="white"><p style={{color:'wheat'}}><FaChessPawn size={35}/></p></td>
+        </tr>
+        <tr>
+        <td class="white">
+            <p style={{color:'wheat'}}><FaChessRook size={35}/></p> </td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessKnight size={35} /></p></td>
+            <td class="white"><p style={{color:'wheat'}}><FaChessBishop size={35}/></p></td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessQueen size={35}/></p></td>
+            <td class="white"><p style={{color:'wheat'}}><FaChessKing size={35}/></p></td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessBishop size={35}/></p></td>
+            <td class="white"><p style={{color:'wheat'}}><FaChessKnight size={35}/></p></td>
+            <td class="black"><p style={{color:'wheat'}}><FaChessRook size={35}/></p></td>
+        </tr>
+    </table>
     </div>
   );
 }
 
-export default App;
+export default Chessboard;
+
